910cb150de85ccbdde675c300e66644941c4942a
JeffreyGaydos <jeff13gaydos@gmail.com>
2026-06-26 23:12:40 -0400

    simplify web server a bit

--- web-server.js
+++ web-server.js
@@ -4,15 +4,11 @@ import { Landing, Tagging } from './controller.js';
 
 export async function GetWebResource(url, mime, res) {
     fs.readFile('./' + url, function(err, data) {
-        RouteEndpoints(url, data).then(apiData => {
-            if(apiData) {
-                mime = 'text/html';
+        RouteEndpoints(url, data).then(modifiedData => {
+            if(modifiedData.modified || !err) {
+                mime = modifiedData.modified ? 'text/html' : mime;
                 res.setHeader('Content-type', mime);
-                res.end(apiData);
-            }
-            else if (!err) {
-                res.setHeader('Content-type', mime);
-                res.end(data);
+                res.end(modifiedData.apiData);
             } else {
                 console.log(`file not found: "${url}"`);
                 res.writeHead(404, "Not Found");
@@ -29,23 +25,32 @@ function WithTypicalResponseContainer(jsonData) {
 
 async function RouteEndpoints(url, currentData) {
     const urlBits = url.split("/").filter(b => b !== '');
-    console.log({ urlBits });
     switch(urlBits[0]) {
         case "ui":
             switch(urlBits[1]) {
                 case "landing.html":
                     const landingData = await Landing();
-                    return `${currentData}${WithTypicalResponseContainer(landingData)}`;;
+                    console.log(landingData);
+                    return {
+                        apiData: `${currentData}${WithTypicalResponseContainer(landingData)}`,
+                        modified: true
+                    };
                 case "tagging":
                     if(urlBits[2]?.match(/[1-9]+[0-9]*/)) {
                         //Since this endpoint doesn't reference 1 page, but many, we have to grab base data
                         let baseHtml = await BasicGetFile(`./${urlBits[0]}/${urlBits[1]}.html`);
                         const taggingData = await Tagging(urlBits[2], baseHtml);
                         
-                        return `${taggingData.modifiedBaseHtml}${WithTypicalResponseContainer(taggingData.apiData)}`;
+                        return {
+                            apiData: `${taggingData.modifiedBaseHtml}${WithTypicalResponseContainer(taggingData.apiData)}`,
+                            modified: true
+                        };
                     }
             }
             break;
     }
-    return undefined; //no data to add to existing files
+    return {
+        apiData: currentData,
+        modified: false
+    };
 }
