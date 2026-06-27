f1ff48b9214b7bf138790de6520ad4cc27f2baf5
JeffreyGaydos <jeff13gaydos@gmail.com>
2026-06-27 17:27:58 -0400

    prune test and deprecated files

--- ui/sequence-form/sequence-form.js
+++
@@ -1,8 +0,0 @@
-console.log("Sequence form");
-
-document.querySelector("#tag-type").addEventListener("change", (e) => {
-    var revealOption = e.target.value;
-    console.log(revealOption);
-    var selector = '.revealable[by="' + revealOption + '"]';
-    // document.querySelector(selector)?.style.display = "block";
-});
